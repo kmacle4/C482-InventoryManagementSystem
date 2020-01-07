a8280dd1f126eaca974d1ce786888aeac3139858
Kirk MacLean <Kirk.maclean@ymail.com>
2020-01-07 15:47:02 -0600

    Update AddProductMenuController.java
    Fixed some Features on the Add Product screen

--- KirkMacLean_Application/src/controller/AddProductMenuController.java
+++ KirkMacLean_Application/src/controller/AddProductMenuController.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,6 +23,11 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Product;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Inventory;
+import model.Part;
 
 /**
  * FXML Controller class
@@ -29,20 +36,44 @@ import javafx.stage.Stage;
  */
 public class AddProductMenuController implements Initializable {
 
-    @FXML
+     @FXML
     private Button searchProductBtn;
 
     @FXML
     private TextField searchProductText;
 
     @FXML
-    private TableView<?> addProductTableView;
+    private TableView<Part> topTableView;
+
+    @FXML
+    private TableColumn<Part, Integer> partsIdCol;
+
+    @FXML
+    private TableColumn<Part, String> partNameCol;
+
+    @FXML
+    private TableColumn<Part, Integer> partInvCol;
+
+    @FXML
+    private TableColumn<Part, Double> partPriceCol;
 
     @FXML
     private Button addProductBtn;
 
     @FXML
-    private TableView<?> deleteProductTableView;
+    private TableView<Part> bottomTableView;
+
+    @FXML
+    private TableColumn<Part, Integer> associatedIdCol;
+
+    @FXML
+    private TableColumn<Part, String> associatedNameCol;
+
+    @FXML
+    private TableColumn<Part, Integer> associatedInvCol;
+
+    @FXML
+    private TableColumn<Part, Double> associatedPriceCol;
 
     @FXML
     private Button deleteProductBtn;
@@ -73,10 +104,20 @@ public class AddProductMenuController implements Initializable {
     
     Stage stage;
     Parent scene;
+    private int counter;
+    
+    ObservableList<Part> search = FXCollections.observableArrayList();
+    ObservableList<Part> associated = FXCollections.observableArrayList();
 
     @FXML
     void onActionAddProduct(ActionEvent event) {
-
+        associated.add(topTableView.getSelectionModel().getSelectedItem());
+        Part part = topTableView.getSelectionModel().getSelectedItem();
+        bottomTableView.setItems(associated);
+        associatedIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        associatedNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        associatedInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        associatedPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
     }
 
     @FXML
@@ -101,24 +142,105 @@ public class AddProductMenuController implements Initializable {
 
     @FXML
     void onActionDeleteProduct(ActionEvent event) {
-
+        associated.remove(bottomTableView.getSelectionModel().getSelectedItem());
     }
 
     @FXML
-    void onActionSaveProduct(ActionEvent event) {
+    void onActionSaveProduct(ActionEvent event) throws IOException {
+        
+        String name = addProductNameText.getText();
+        double price = Double.parseDouble(addProductPriceText.getText());
+        int stock = Integer.parseInt(addProductInvText.getText());
+        int min = Integer.parseInt(addProductMinText.getText());
+        int max = Integer.parseInt(addProductMaxText.getText());
+        
+        Product product = new Product(counter, name, price, stock, min, max);
+        try{
+            if(addProductNameText.getText().equals("")){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning");
+                alert.setContentText("Missing Name");
+                alert.showAndWait();
+            }
+            else if(min > max){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning");
+                alert.setContentText("Minimum is greater tha Maximum");
+                alert.showAndWait();
+            }
+            else{
+                Inventory.addProduct(product);
+                associated.clear();
+            }
+        }
+        catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("WARNING");
+            alert.setContentText("Missing Data");
+            alert.showAndWait();
+        }
+        
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+            
+        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
 
+        stage.setScene(new Scene(scene));
+
+        stage.show();
     }
 
     @FXML
     void onActionSearchProduct(ActionEvent event) {
-
+        try {
+            try{
+                int id = Integer.parseInt(searchProductText.getText());
+                search.add(Inventory.lookupPart(id));
+            }
+            catch(Exception e){
+                String name = searchProductText.getText();
+                search = Inventory.lookupPart(name);
+            }
+            if(search.size() == 0){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning");
+                alert.setContentText("Missing Part");
+            }
+            else {
+                topTableView.setItems(search);
+                partsIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+                partNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+                partInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
+                partPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+            }
+        
+        }
+        catch(Exception e){
+           Alert alert = new Alert(Alert.AlertType.WARNING);
+           alert.setTitle("Warning");
+           alert.setContentText("Missing Part"); 
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
         
+        //Top Table
+        partsIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        partNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        partInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        partPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
         
+        //Product Table
+        associatedIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        associatedNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        associatedInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        associatedPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+        
+        //Update Table
+        topTableView.setItems(Inventory.getAllParts());
+        
+        counter = Inventory.getProductCounter();
+        addProductIdText.setText(Integer.toString(counter));
 
     }  
     
